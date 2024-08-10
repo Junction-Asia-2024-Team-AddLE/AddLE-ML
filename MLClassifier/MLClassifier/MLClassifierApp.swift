434e32310e8a2c57a39275f7feb3c49a19825433
shinseungjae <mok05247@naver.com>
2024-08-10 21:04:16 +0900

    feat: 프로젝트 파일 새로 import

---
+++ MLClassifier/MLClassifier/MLClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLClassifierApp.swift
+//  MLClassifier
+//
+//  Created by 신승재 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MLClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
